13284a8b9213b009e22aa5944e941aa3acacf369
Alexander Brickwedde <alex@brickwedde.de>
2010-12-30 01:26:39 +0100

    x

---
+++ wordclock-bootloader/Debug/main.d
@@ -0,0 +1,3 @@
+main.d main.o: ../main.c ../uart.h
+
+../uart.h:
---
+++ wordclock-bootloader/Debug/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../main.c \
+../uart.c 
+
+OBJS += \
+./main.o \
+./uart.o 
+
+C_DEPS += \
+./main.d \
+./uart.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: AVR Compiler'
+	avr-gcc -Wall -g2 -gstabs -O0 -fpack-struct -fshort-enums -std=gnu99 -funsigned-char -funsigned-bitfields -mmcu=atmega32 -DF_CPU=16000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
