d616fa9a1ff793ed4ab323859834992169f24c3d
Alexander Brickwedde <alex@brickwedde.de>
2011-01-30 13:48:35 +0100

    x

--- wordclock/Release/i2c-master.d
+++ wordclock/Release/i2c-master.d
@@ -1,4 +1,49 @@
-i2c-master.d i2c-master.o: ../i2c-master.c ../uart.h ../i2c-master.h
+i2c-master.d i2c-master.o: ../i2c-master.c \
+  C:\WinAVR-20100110\avr\include/inttypes.h \
+  C:\WinAVR-20100110\avr\include/stdint.h \
+  C:\WinAVR-20100110\avr\include/compat/twi.h \
+  C:\WinAVR-20100110\avr\include/util/twi.h \
+  C:\WinAVR-20100110\avr\include/avr/io.h \
+  C:\WinAVR-20100110\avr\include/avr/sfr_defs.h \
+  C:\WinAVR-20100110\avr\include/avr/iom32.h \
+  C:\WinAVR-20100110\avr\include/avr/portpins.h \
+  C:\WinAVR-20100110\avr\include/avr/common.h \
+  C:\WinAVR-20100110\avr\include/avr/version.h \
+  C:\WinAVR-20100110\avr\include/avr/fuse.h \
+  C:\WinAVR-20100110\avr\include/avr/lock.h \
+  C:\WinAVR-20100110\avr\include/util/delay.h \
+  C:\WinAVR-20100110\avr\include/util/delay_basic.h \
+  C:\WinAVR-20100110\avr\include/stdio.h ../uart.h ../i2c-master.h
+
+C:\WinAVR-20100110\avr\include/inttypes.h:
+
+C:\WinAVR-20100110\avr\include/stdint.h:
+
+C:\WinAVR-20100110\avr\include/compat/twi.h:
+
+C:\WinAVR-20100110\avr\include/util/twi.h:
+
+C:\WinAVR-20100110\avr\include/avr/io.h:
+
+C:\WinAVR-20100110\avr\include/avr/sfr_defs.h:
+
+C:\WinAVR-20100110\avr\include/avr/iom32.h:
+
+C:\WinAVR-20100110\avr\include/avr/portpins.h:
+
+C:\WinAVR-20100110\avr\include/avr/common.h:
+
+C:\WinAVR-20100110\avr\include/avr/version.h:
+
+C:\WinAVR-20100110\avr\include/avr/fuse.h:
+
+C:\WinAVR-20100110\avr\include/avr/lock.h:
+
+C:\WinAVR-20100110\avr\include/util/delay.h:
+
+C:\WinAVR-20100110\avr\include/util/delay_basic.h:
+
+C:\WinAVR-20100110\avr\include/stdio.h:
 
 ../uart.h:
 
--- wordclock/i2c-rtc.c
+++ wordclock/i2c-rtc.c
@@ -315,10 +315,17 @@ sub_minute(DATETIME * datetime)
 const uint8_t amziffer[12] =
 { 0, 3, 3, 6, 1, 4, 6, 2, 5, 0, 3, 5 };
 
-const int arise[] =
-{ 0, 675, 650, 560, 450, 330, 230, 240, 300, 400, 490, 570, 650, 675 };
-const int afall[] =
-{ 0, 1530, 1620, 1700, 1800, 1880, 1970, 1990, 1920, 1820, 1700, 1600, 1530, 1540 };
+#define XY2CL(x,y) (x*100 + ((y*6)/10))
+
+#define RISE(x,y) (XY2CL(x,y) - XY2CL(1,15))
+#define FALL(x,y) (XY2CL(x,y) - XY2CL(1,30))
+
+//
+// Sunset-Timing in local time (non-dst!) / adjust utc-offset in RISE/FALL macro
+//
+//                       JAN          FEB          MAR          APR          MAI          JUN          JUL          AUG          SEP          OKT          NOV          DEZ          JAN
+const int arise[] = { 0, RISE( 8,37), RISE( 8,12), RISE( 7,20), RISE( 6,11), RISE( 5, 8), RISE( 4,52), RISE( 4,20), RISE( 4,55), RISE( 5,43), RISE( 6,31), RISE( 7,23), RISE( 8,12), RISE( 8,37) };
+const int afall[] = { 0, FALL(16,34), FALL(17,21), FALL(18,11), FALL(19, 3), FALL(19,52), FALL(20,37), FALL(20,52), FALL(20,22), FALL(19,21), FALL(18,13), FALL(17, 9), FALL(16,29), FALL(16,34) };
 
 uint8_t
 i2c_rtc_read(DATETIME * datetime, uint8_t bLocal)
