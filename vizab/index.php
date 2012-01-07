1c1c341e4d0dc3ab248fa66d896489f7824c4e30
Alexander Brickwedde <alex_floodping@brickwedde.de>
2012-01-07 22:40:36 +0100

    x

--- vizab/index.php
+++ vizab/index.php
@@ -27,30 +27,43 @@ document.getElementById("graph4").src="/intern/munin/localdomain/localhost.local
 <img style="position:absolute;top:210px;left:410px" src="png/transparent.png" id="WZHeizung">
 <input type="text" style="position:absolute;top:100px;left:400px;width:45px" id="WZSofaText">
 
-<input onclick="showgraph('0001');" style="border:0px;background-color:transparent;position:absolute;top:100px;left:170px;width:60px;" unit="°C" type="text" value="x" name="0001" id="airid0001">
-<input onclick="showgraph('0002');" style="border:0px;background-color:transparent;position:absolute;top:330px;left:70px;width:60px;" unit="°C" type="text" value="x" name="0002" id="airid0002">
-<input onclick="showgraph('0003');" style="border:0px;background-color:transparent;position:absolute;top:380px;left:400px;width:60px;" unit="°C" type="text" value="x" name="0003" id="airid0003">
-<input onclick="showgraph('0004');" style="border:0px;background-color:transparent;position:absolute;top:210px;left:470px;width:60px;" unit="°C" type="text" value="x" name="0004" id="airid0004">
-<input onclick="showgraph('0005');" style="border:0px;background-color:transparent;position:absolute;top:470px;left:400px;width:60px;" unit="°C" type="text" value="x" name="0005" id="airid0005">
-<input onclick="showgraph('0101');" style="border:0px;background-color:transparent;position:absolute;top:30px;left:540px;width:60px;" unit="cm" type="text" value="x" name="0101" id="airid0101">
-<input onclick="showgraph('0102RH');" style="border:0px;background-color:transparent;position:absolute;top:50px;left:540px;width:60px;" unit="%" type="text" value="x" name="0102RH" id="airid0102RH">
-<input onclick="showgraph('0102P');" style="border:0px;background-color:transparent;position:absolute;top:70px;left:540px;width:60px;" unit="hPa" type="text" value="x" name="0102P" id="airid0102P">
-<input onclick="showgraph('0102T');" style="border:0px;background-color:transparent;position:absolute;top:90px;left:540px;width:60px;" unit="°C" type="text" value="x" name="0102T" id="airid0102T">
-<input onclick="showgraph('0102D');" style="border:0px;background-color:transparent;position:absolute;top:110px;left:540px;width:60px;" unit="°C" type="text" value="x" name="0102D" id="airid0102D">
+<input style="border:0px;background-color:transparent;position:absolute;top:100px;left:170px;width:60px;" unit="°C" type="text" value="x" name="0001" id="airid0001">
+<input style="border:0px;background-color:transparent;position:absolute;top:330px;left:70px;width:60px;" unit="°C" type="text" value="x" name="0002" id="airid0002">
+<input style="border:0px;background-color:transparent;position:absolute;top:380px;left:400px;width:60px;" unit="°C" type="text" value="x" name="0003" id="airid0003">
+<input style="border:0px;background-color:transparent;position:absolute;top:210px;left:470px;width:60px;" unit="°C" type="text" value="x" name="0004" id="airid0004">
+<input style="border:0px;background-color:transparent;position:absolute;top:470px;left:400px;width:60px;" unit="°C" type="text" value="x" name="0005" id="airid0005">
+
+<input style="border:0px;background-color:transparent;position:absolute;top:30px;left:540px;width:60px;" unit="cm" type="text" value="x" name="0101" id="airid0101">
+<input style="border:0px;background-color:transparent;position:absolute;top:60px;left:540px;width:60px;" unit="%" type="text" value="x" name="0102RH" id="airid0102RH">
+<input style="border:0px;background-color:transparent;position:absolute;top:90px;left:540px;width:60px;" unit="hPa" type="text" value="x" name="0102P" id="airid0102P">
+<input style="border:0px;background-color:transparent;position:absolute;top:120px;left:540px;width:60px;" unit="°C" type="text" value="x" name="0102T" id="airid0102T">
+<input style="border:0px;background-color:transparent;position:absolute;top:150px;left:540px;width:60px;" unit="°C" type="text" value="x" name="0102D" id="airid0102D">
+
+<img onclick="showgraph('0001');" style="position:absolute;top:100px;left:230px" src="graph.gif">
+<img onclick="showgraph('0002');" style="position:absolute;top:330px;left:130px" src="graph.gif">
+<img onclick="showgraph('0003');" style="position:absolute;top:380px;left:460px" src="graph.gif">
+<img onclick="showgraph('0004');" style="position:absolute;top:210px;left:530px" src="graph.gif">
+<img onclick="showgraph('0005');" style="position:absolute;top:470px;left:460px" src="graph.gif">
+
+<img onclick="showgraph('0101');" style="position:absolute;top:30px;left:600px" src="graph.gif">
+<img onclick="showgraph('0102RH');" style="position:absolute;top:60px;left:600px" src="graph.gif">
+<img onclick="showgraph('0102P');" style="position:absolute;top:90px;left:600px" src="graph.gif">
+<img onclick="showgraph('0102T');" style="position:absolute;top:120px;left:600px" src="graph.gif">
+<img onclick="showgraph('0102D');" style="position:absolute;top:150px;left:600px" src="graph.gif">
 
 <span style="position:absolute;top:34px;left:460px">Wasserstand</span>
-<span style="position:absolute;top:54px;left:460px">Luftfeuchte</span>
-<span style="position:absolute;top:74px;left:460px">Luftdruck</span>
-<span style="position:absolute;top:94px;left:460px">Temperatur</span>
-<span style="position:absolute;top:114px;left:460px">Taupunkt</span>
+<span style="position:absolute;top:64px;left:460px">Luftfeuchte</span>
+<span style="position:absolute;top:94px;left:460px">Luftdruck</span>
+<span style="position:absolute;top:124px;left:460px">Temperatur</span>
+<span style="position:absolute;top:154px;left:460px">Taupunkt</span>
 
 <img style="position:absolute;top:700px;left:20px;width:495px" src="png/transparent.png" id="graph1">
 <img style="position:absolute;top:970px;left:20px;width:495px" src="png/transparent.png" id="graph2">
 <img style="position:absolute;top:1240px;left:20px;width:495px" src="png/transparent.png" id="graph3">
 <img style="position:absolute;top:1510px;left:20px;width:495px" src="png/transparent.png" id="graph4">
 
-<a style="position:absolute;top:170px;left:500px" href="/icinga/">Icinga</a>
-<a style="position:absolute;top:190px;left:500px" href="/intern/munin/localdomain/localhost.localdomain/index.html#sensoren">Munin</a>
+<a style="position:absolute;top:170px;left:50px" href="/icinga/">Icinga</a>
+<a style="position:absolute;top:190px;left:50px" href="/intern/munin/localdomain/localhost.localdomain/index.html#sensoren">Munin</a>
 
 </body>
 </html>
